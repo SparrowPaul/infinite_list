306c0fb02f8410de76bc06616e255e3017e7ce8d
sparrow <sparrowpaul2@gmail.com>
2022-05-01 15:00:44 +0000

    Initial commit

---
+++ lib/posts/bloc/post_event.dart
@@ -0,0 +1,11 @@
+part of 'post_bloc.dart';
+
+abstract class PostEvent extends Equatable {
+  const PostEvent();
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
+
+class PostFetched extends PostEvent {}
